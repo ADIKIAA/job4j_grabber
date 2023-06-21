ce2e38294bdd56277edfb96afbb1ab13e922c667
ADIKIAA <zhekov.nick@yandex.ru>
2023-06-22 00:29:53 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -4,9 +4,11 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.List;
 
 public class HabrCareerParse {
 
@@ -23,11 +25,25 @@ public class HabrCareerParse {
                 Element titleElement = row.select(".vacancy-card__title").first();
                 Element linkElement = titleElement.child(0);
                 Element dateElement = row.select(".vacancy-card__date").first();
+                String description;
+                try {
+                    description = retrieveDescription(linkElement.attr("href"));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
                 String vacancyName = titleElement.text();
                 String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
                 String date = dateElement.text();
-                System.out.printf("%s %s %s%n", vacancyName, link, date);
+                System.out.printf("%s %s %s %s%n", vacancyName, link, date, description);
             });
         }
     }
+
+    private static String retrieveDescription(String link) throws IOException {
+        Connection connection = Jsoup.connect(String.format("%s%s", SOURCE_LINK, link));
+        Document document = connection.get();
+        Elements desc = document.select(".vacancy-description__text").first().getAllElements();
+        return desc.text();
+    }
 }
+
