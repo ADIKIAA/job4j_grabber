d0769277807a13d8afdb21723cd463bb189cbdac
ADIKIAA <zhekov.nick@yandex.ru>
2023-06-23 21:16:44 +0300

    2.1. Преобразование даты [#289476 #424422]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import static java.time.format.DateTimeFormatter.ISO_DATE_TIME;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
-
     @Override
     public LocalDateTime parse(String parse) {
         return LocalDateTime.parse(parse, ISO_DATE_TIME);
